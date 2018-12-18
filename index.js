d2e2b6aedb2c4d3a26f4337ab47269f40443144f
Mitchell Snow <snow801mitch@gmail.com>
2018-12-18 11:31:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return ("Happy holidays, ${name}!")
+  return ("Happy holidays, you!")
 }
 
 function happyHolidaysTo(holiday, name) {
